65257c9e54537b056d1b0932c457fda08ac34f13
Mahmud Hasan <28367258+QuodFinis@users.noreply.github.com>
2023-12-11 18:42:31 -0500

    created venv and my neural net folder

---
+++ Neural-Network_Mahmud-Hasan/neuralnet.py
@@ -0,0 +1,15 @@
+# 28 x 28 pixel image
+# as the images are greyscale, each pixel has value 0 (black) to 255 (white)
+
+# represent this first as a matrix, each row being an example and having 784 columns
+# corresponding to each pixel in the image
+
+# transpose this matrix such that each column is an example and each column has 784 rows
+
+# input layer: 784 nodes for each pixel
+# hiden layer: 10 nodes
+# ouput laye: 10 nodes for each possible digit classification
+
+# forward propegation
+# running image through the network to see what network outputs
+#
